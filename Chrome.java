f6480eba1819534c2bb73cb59be1c1ad900dc26f
nivedhithaela <87236547+nivedhithaela@users.noreply.github.com>
2024-11-20 20:24:55 +0530

    Add files via upload
    DataTypes Home Assignment

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println(version+" "+developer+" "+isBeta+" "+releaseYear+" "+shortcutKey);
+		System.out.println("");
+		
+
+	}
+
+}
