97e06f7ac0c16f265edba4e6f87cdbb37e50ae89
nivedhithaela <87236547+nivedhithaela@users.noreply.github.com>
2024-11-25 15:47:33 +0530

    Add files via upload

---
+++ day3/LibraryManagement.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library lbobj=new Library();
+		System.out.println(lbobj.addBook("Alchemist"));
+		lbobj.issueBook();
+
+	}
+
+}
