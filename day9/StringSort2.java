ba3d2f18156f94040c7bba373c7c063e58d3aa1c
nivedhithaela <87236547+nivedhithaela@users.noreply.github.com>
2024-12-09 10:11:14 +0530

    Add files via upload

---
+++ day9/StringSort2.java
@@ -0,0 +1,30 @@
+package week3.day3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StringSort2 {
+
+	public static void main(String[] args) {
+		List<String> comp=new ArrayList<String>();
+		//adding elements
+		comp.add("HCL");
+		comp.add("Wipro");
+		comp.add("Aspire Systems");
+		comp.add("CTS");
+		System.out.println("Before Sort"+comp);
+		//size
+		int comp_size=comp.size();
+		System.out.println("size is:"+comp_size);
+		//sort
+		Collections.sort(comp);
+		System.out.println("After Sort"+comp);
+		Collections.reverse(comp);
+		System.out.println("Reverse Order is:"+comp);
+		
+		
+
+	}
+
+}
